7e82c58a38e531b21371446ed9823c02d7e1e259
MelimeliDev <melisa.surja.pro@gmail.com>
2021-04-07 21:18:52 +0800

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -4,7 +4,7 @@ import Link from 'next/link'
 const Layout = ({children}) => {
     return (
         <VStack h="100vh" align="stretch">
-            <Box bg="#000" py=".1em">
+            <Box bg="#000" py=".1em" px="2rem">
                 <Container maxW="container.lg">
                     <Flex>
                         <Box><a href="https://eleuther.ai"><img src="/logo.jpg" /></a></Box>
@@ -17,7 +17,7 @@ const Layout = ({children}) => {
                     </Flex>
                 </Container>
             </Box>
-            <Center h="100%">
+            <Center h="100%" px="2rem">
                 <Container maxW="container.lg">
                     {children}
                 </Container>
