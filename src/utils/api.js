26d6a95dfe6a29e64e4fb824382810b9bbc18a04
kip <kipgparker@gmail.com>
2021-05-22 20:16:50 +0100

    Minimum example working

--- src/utils/api.js
+++ src/utils/api.js
@@ -12,6 +12,22 @@ export const getComparison = async () => {
     const response = await fetch('http://localhost:8000/api/v1/filter/comparisons', requestOptions);
     const data = await response.json();
 
-    console.log(data);
+    return data;
+};
+
+export const updateComparison = async (item_1_is_better, id) => {
+    
+    const requestOptions = {
+        method: 'PUT',
+        headers: {
+            "accept": "application/json",
+            'Authorization': 'Bearer ' + localStorage.getItem('token')
+        },
+        body: JSON.stringify({ item_1_is_better: item_1_is_better, id: id})
+    };
+
+    const response = await fetch('http://localhost:8000/api/v1/filter/comparisons', requestOptions);
+    const data = await response.json();
+
     return data;
 };
--- src/views/project/Filter.js
+++ src/views/project/Filter.js
@@ -5,146 +5,112 @@ import {
     Heading,
     Text,
     Button,
-    useRadioGroup,
+    RadioGroup,
+    Radio,
     Center,
     CircularProgress
 } from "@chakra-ui/react";
 import { ArrowForwardIcon } from "@chakra-ui/icons";
 import BackHomeButton from "../../components/BackHomeButton";
-import RadioCard from "../../components/RadioCard";
-import { getComparison } from "../../utils/api"
+import RadioCard from "../../components/RadioCard"; 
+// TODO use RadioCard again, i just couldn't figure out how to integrate it
+import { getComparison, updateComparison } from "../../utils/api"
 
 export default function CompareText() {
-    const summaries = [
-        {
-            textToSummarize:
-                "Lorem Ipsum 1 is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-            summaries: [
-                "This is the summary 1 of the text number 1. Please choose this summary if you think its the best. Once you decided just click on it. Have a nice day!",
-                "This is the summary 2 of the text number 1. Please choose this summary if you think its the best. Once you decided just click on it. Have a nice day!"
-            ]
-        },
-        {
-            textToSummarize:
-                "Lorem Ipsum 2 is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-            summaries: [
-                "This is the summary 1 of the text number 2. Please choose this summary if you think its the best. Once you decided just click on it. Have a nice day!",
-                "This is the summary 2 of the text number 2. Please choose this summary if you think its the best. Once you decided just click on it. Have a nice day!"
-            ]
-        },
-        {
-            textToSummarize:
-                "Lorem Ipsum 3 is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-            summaries: [
-                "This is the summary 1 of the text number 3. Please choose this summary if you think its the best. Once you decided just click on it. Have a nice day!",
-                "This is the summary 2 of the text number 3. Please choose this summary if you think its the best. Once you decided just click on it. Have a nice day!"
-            ]
-        }
-    ];
-    const [chosenSummary, setChosenSummary] = useState("");
-    const [confirmedChoices, setConfirmedChoices] = useState([]);
-    const { getRootProps, getRadioProps } = useRadioGroup({
-        name: "summary",
-        onChange: setChosenSummary
-    });
-    const [currentNumberKey, setCurrentNumberKey] = useState();
+   
+    const [page, setPage] = useState(1);
+    const [nextSample, setNextSample] = useState([]);
+    const [sample, setSample] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
-    const group = getRootProps();
+    const [currentNumberKey, setCurrentNumberKey] = useState(-1);
+
+    useEffect(() => {
+        getComparison().then(res => {
+            console.log(res)
+            setNextSample(res);
+            setSample(nextSample);
+            setIsLoading(false);
+        }).catch(error => console.log(error));
+    }, [page])
+
     const confirmChoice = async () => {
         setIsLoading(true);
-        const data = await getComparison()
+        const data = await updateComparison(
+            currentNumberKey == "0" ? true : false,
+            sample.comparison_id
+        )
         setIsLoading(false);
-        //if (chosenSummary == "") return;
-        //console.log("You picked: " + chosenSummary);
-        //setConfirmedChoices([...confirmedChoices, chosenSummary]);
-        //setChosenSummary("");
+        setCurrentNumberKey(-1)
+        setPage(page + 1)
     };
 
+    
     useEffect(() => {
         function handleKeyDown(e) {
             const char = e.which || e.keyCode;
             if (e.code === "Space") {
                 confirmChoice();
             } else if (char === 49) {
-                setCurrentNumberKey(1);
-                setChosenSummary(
-                    summaries[confirmedChoices.length].summaries[0]
-                );
+                setCurrentNumberKey("0");
             } else if (char === 50) {
-                setCurrentNumberKey(2);
-                setChosenSummary(
-                    summaries[confirmedChoices.length].summaries[1]
-                );
+                setCurrentNumberKey("1");
             }
         }
-
         window.addEventListener("keydown", handleKeyDown);
-
         return function cleanup() {
             window.removeEventListener("keydown", handleKeyDown);
         };
-    }, [setChosenSummary, chosenSummary, setCurrentNumberKey]);
+    }, [setCurrentNumberKey, currentNumberKey]);
 
     return (
         <VStack align="start" spacing={8}>
             <BackHomeButton />
-            {confirmedChoices.length < summaries.length ? (
+            {page == 1 ? (
                 <>
-                    <Heading textAlign="center" size="lg" width="100%">
-                        Summarisation Experiment
-                    </Heading>
-                    <Heading size="md" width="100%" textAlign="center">
-                        Please choose the better summarisation of the text above
-                    </Heading>
-                    <VStack align="start" spacing={2} {...group}>
-                        {summaries[confirmedChoices.length].summaries.map(
-                            (value, i) => {
-                                const radio = getRadioProps({ value });
-                                return (
-                                    <RadioCard
-                                        key={i}
-                                        {...radio}
-                                        isChecked={
-                                            chosenSummary &&
-                                            currentNumberKey === i + 1
-                                        }
-                                        autoFocus={true}
-                                    >
-                                        {value}
-                                    </RadioCard>
-                                );
-                            }
-                        )}
-                    </VStack>
+                    <Heading size="lg">Thanks for helping out</Heading>
+                    <Text>
+                        you will be shown two peices of text
+                        and you have to select which one you prefer, 
+                        once you have pressed submit your choice will be finalized
+                    </Text>
                     <Button
                         rightIcon={<ArrowForwardIcon w={5} h={5} />}
-                        onClick={confirmChoice}
-                        disabled={!chosenSummary}
+                        onClick={() => setPage(page + 1)}
                     >
-                    {isLoading ? (
-                        <CircularProgress
-                        isIndeterminate
-                        size="24px"
-                        />
-                    ) : (
-                        'Confirm'
-                    )}
+                        Get started 
                     </Button>
                 </>
             ) : (
                 <>
-                    <Heading size="lg">Thank you!</Heading>
-                    <Text>You have reached the end of our current demo.</Text>
-                    <Text>These are your choices:</Text>
-                    <VStack
-                        align="start"
-                        spacing={2}
-                        divider={<StackDivider borderColor="blue.700" />}
-                    >
-                        {confirmedChoices.map((value, i) => (
-                            <Text key={i}>{value}</Text>
-                        ))}
+                <Heading textAlign="center" size="lg" width="100%">
+                                        Dataset Filtering Experiment
+                </Heading>
+                <Heading size="md" width="100%" textAlign="center">
+                        Please choose your favourite text
+                </Heading>
+                <RadioGroup onChange={setCurrentNumberKey} value={currentNumberKey}>
+                    <VStack align="start" spacing={2}>
+                        <Radio
+                            key={0}
+                            value={0}
+                        >
+                            {sample.text_sample_1}
+                        </Radio>
+                        <Radio
+                            key={1}
+                            value={1}
+                        >
+                            {sample.text_sample_2}
+                        </Radio>
                     </VStack>
+                </RadioGroup>
+                <Button
+                    rightIcon={<ArrowForwardIcon w={5} h={5} />}
+                    onClick={confirmChoice}
+                    disabled={currentNumberKey == -1}
+                >
+                    Confirm
+                </Button>
                 </>
             )}
         </VStack>
